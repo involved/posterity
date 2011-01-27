ff31153de5ff009d94aea375f75411ff22f8da6a
Nicholas Bruning <nicholas@bruning.com.au>
2011-01-27 15:07:53 +1100

    Styled login form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  layout :layout_by_resource
+  
   # prevents robot indexing
   def no_index
     @no_index = true
+  end
+  
+  protected
+  def layout_by_resource
+    if devise_controller?
+      "authentication"
+    else
+      "application"
+    end
   end
 end
