7ca21892588b71bb5b1f04a2b9f7c17d647d4631
Nicholas Bruning <nicholas@bruning.com.au>
2011-05-02 11:33:32 +1000

    Added permalink stuff

--- lib/posterity/post.rb
+++ lib/posterity/post.rb
@@ -13,6 +13,7 @@ module Posterity
       base.field :content
       base.field :author
       base.field :author_param
+      base.field :permalink
 
       base.slug :title
 
@@ -20,11 +21,13 @@ module Posterity
       base.validates_presence_of :author
 
       base.before_save :update_author_param
+      base.before_create :generate_permalink!
     end
 
     module ClassMethods
       # Need some scopes here for tagged and archived posts
       def tagged(tags)
+        all_in(:tags_array => Array(tags))
       end
 
       def written_by(author)
@@ -45,6 +48,14 @@ module Posterity
         sentences[0] = sentences[0].truncate(options[:letters]) if sentences[0]
 
         sentences.join("").html_safe
+      end
+
+      protected
+      def generate_permalink!
+        self.permalink = ActiveSupport::SecureRandom.hex(4)
+        while self.class.first(:conditions => {:permalink => self.permalink}) do
+          self.permalink = ActiveSupport::SecureRandom.hex(4)
+        end
       end
 
       private
