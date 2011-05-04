7d323e74b3e7023c08685f6cd995c9109059d5df
Nicholas Bruning <nicholas@bruning.com.au>
2011-05-04 13:05:28 +1000

    Updated routes to add custom controller option

--- lib/posterity/routes.rb
+++ lib/posterity/routes.rb
@@ -8,12 +8,12 @@ module Posterity
 
       match "#{path}" => "#{options[:controller]}#index", :as => path, :resources => options[:resources].to_s
       match "#{path}/:year/:month/:day/:id" => "#{options[:controller]}#show", :as => path.to_s.singularize, :resources => options[:resources].to_s
-      match "#{path}/:year/:month/:day" => "#{options[:controller]}#index", :as => "#{path}_archive", :resources => options[:resources].to_s
-      match "#{path}/:year/:month" => "#{options[:controller]}#index", :as => "#{path}_archive", :resources => options[:resources].to_s
-      match "#{path}/:year" => "#{options[:controller]}#index", :as => "#{path}_archive", :resources => options[:resources].to_s
       match "#{path}/written_by/:author" => "#{options[:controller]}#index", :as => "#{path}_written_by", :resources => options[:resources].to_s
       match "#{path}/tagged/:tag" => "#{options[:controller]}#index", :as => "#{path}_tagged", :resources => options[:resources].to_s
       match "#{path}/permalink/:permalink" => "#{options[:controller]}#show", :as => "#{path.to_s.singularize}_permalink", :resources => options[:resources].to_s
+      match "#{path}/:year/:month/:day" => "#{options[:controller]}#index", :as => "#{path}_archive", :resources => options[:resources].to_s
+      match "#{path}/:year/:month" => "#{options[:controller]}#index", :as => "#{path}_archive", :resources => options[:resources].to_s
+      match "#{path}/:year" => "#{options[:controller]}#index", :as => "#{path}_archive", :resources => options[:resources].to_s
     end
   end
 end
